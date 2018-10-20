c36dd290c6e77146537c11c65d78098b0e72a6c0
PaulinaKaplonJimp <paulinakaplon@o2.pl>
2018-10-20 18:11:58 +0200

    adds packages database + functionalities

--- packman/accounts/forms.py
+++ packman/accounts/forms.py
@@ -4,10 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     class Meta:
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
         model = get_user_model()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["username"].label = "Display name"
         self.fields["email"].label = "Email address"
+        self.fields["first_name"].label = "Enter your first name"
+        self.fields["last_name"].label = "enter your last name"
--- packman/packages/models.py
+++ packman/packages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Package(models.Model):
+    sender_id = models.CharField(max_length=150)
+    driver_id = models.CharField(max_length=150)
+    opinion = models.CharField(max_length=500)
+    date_registration = models.DateField()
+    date_start_of_delivery = models.DateField()
+    date_end_of_delivery = models.DateField()
+    state = models.CharField(max_length=20)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return 'Paczka nada przez : %s , zawiezioina przez: %s \n  ' % (self.sender_id, self.driver_id)
