f1726425d78e7aad06a497fc162a346c95f12e66
mmamica <maria.anna.mamica@gmail.com>
2018-10-20 22:58:09 +0200

    maile

--- packman/accounts/views.py
+++ packman/accounts/views.py
@@ -2,10 +2,32 @@ from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
 # from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
 
 from . import forms
+from . import models
+
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
     success_url = reverse_lazy("login")
     template_name = "accounts/signup.html"
+
+
+@csrf_exempt
+def send(request):
+    message=request.user.username + request.user.
+    print(message)
+    send_mail(
+    'kupa',
+    message,
+    'pacman.package.sender@gmail.com',
+    ['kartytko@gmail.com','elakrz@gmail.com','kaplonpaulina97@gmail.com','maria.anna.mamica@gmail.com'],)
+    return HttpResponse("return this string")
+
